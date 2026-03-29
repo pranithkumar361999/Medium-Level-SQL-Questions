1982ec3cb57efc2f5d79ddce41fe9932faa79733
PRANITH KUMAR V <54210519+pranithkumar361999@users.noreply.github.com>
2026-03-29 20:27:03 +0530

    Create No of people present in hospital.sql

---
+++ No of people present in hospital.sql
@@ -0,0 +1,30 @@
+DDL:
+create table hospital ( emp_id int
+, action varchar(10)
+, time datetime);
+
+insert into hospital values ('1', 'in', '2019-12-22 09:00:00');
+insert into hospital values ('1', 'out', '2019-12-22 09:15:00');
+insert into hospital values ('2', 'in', '2019-12-22 09:00:00');
+insert into hospital values ('2', 'out', '2019-12-22 09:15:00');
+insert into hospital values ('2', 'in', '2019-12-22 09:30:00');
+insert into hospital values ('3', 'out', '2019-12-22 09:00:00');
+insert into hospital values ('3', 'in', '2019-12-22 09:15:00');
+insert into hospital values ('3', 'out', '2019-12-22 09:30:00');
+insert into hospital values ('3', 'in', '2019-12-22 09:45:00');
+insert into hospital values ('4', 'in', '2019-12-22 09:45:00');
+insert into hospital values ('5', 'out', '2019-12-22 09:40:00');
+
+Solution:
+with cte as (
+select emp_id, max(time) as time
+from hospital
+group by 1
+),
+cte2 as(
+select a.emp_id, a.time from 
+cte a left join hospital b
+on a.emp_id=b.emp_id
+and a.time=b.time
+where action='in')
+select count(*) from cte2
